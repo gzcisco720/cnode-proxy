f6c20bd13ceebb4b8146e82515a1f0d7d58c26dd
Eric <gzcisco720@gmail.com>
2018-04-03 14:55:02 +1000

    project basic config

---
+++ build/webpack.config.server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    target: "node",
+    entry: {
+        app: path.join(__dirname, '../client/server-entry.js')
+    },
+    output: {
+        filename: 'server.js',
+        path: path.join(__dirname, '../dist'),
+        libraryTarget: "commonjs2"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            },
+        ]
+    }
+}
