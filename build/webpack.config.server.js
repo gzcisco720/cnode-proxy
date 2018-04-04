6546404baff2ccf54e98e5500ef4359b16745177
Eric <gzcisco720@gmail.com>
2018-04-04 10:51:57 +1000

    eslint config

--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -15,6 +15,14 @@ module.exports = {
     module: {
         rules: [
             {
+                enforce: 'pre',
+                test: /.(js|jsx)$/,
+                loader: 'eslint-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            },
+            {
                 test:/\.jsx$/,
                 loader: 'babel-loader'
             },
