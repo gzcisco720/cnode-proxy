05a4618088de74f03a0b804f77b3b997ed802aac
Eric <gzcisco720@gmail.com>
2018-04-04 11:40:56 +1000

    eslint and editorconfig

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,3 @@
 import React from 'react';
 
-export default class App extends React.Component {
-    render(){
-        return (
-          <div>This is a new project</div>
-        );
-    }
-}
+export default () => <div>This is a new project</div>;
--- client/app.js
+++ client/app.js
@@ -1,22 +1,24 @@
+import { AppContainer } from 'react-hot-loader';//eslint-disable-line
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.jsx';
-import { AppContainer } from 'react-hot-loader'
 
 const root = document.getElementById('root');
-const render = Component => {
-    ReactDom.hydrate(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-    root);
-}
+const render = (Component) => {
+  ReactDom.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>
+    ,
+    root,
+  );
+};
 
 render(App);
 
-if(module.hot){
-    module.hot.accept('./App.jsx', () => {
-        const NextApp = require('./App.jsx').default;
-        render(NextApp);
-    });
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default;
+    render(NextApp);
+  });
 }
