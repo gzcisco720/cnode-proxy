506687b89ee8270f544b00632fabe9e1384aa6ff
Eric <gzcisco720@gmail.com>
2018-04-04 11:59:47 +1000

    webpack structure imporvement

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,68 +1,45 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+const isDev = process.env.NODE_ENV === 'development'
 
-const isDev = process.env.NODE_ENV === 'development';
+const config = webpackMerge(baseConfig, {
+  entry: {
+    app: path.join(__dirname, '../client/app.js')
+  },
+  output: {
+    filename: '[name].[hash].js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, '../client/template.html')
+    })
+  ]
+})
 
-const config = {
-    entry: {
-        app: path.join(__dirname, '../client/app.js')
-    },
-    output: {
-        filename: '[name].[hash].js',
-        path: path.join(__dirname, '../dist'),
-        publicPath: '/public/'
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /.(js|jsx)$/,
-                loader: 'eslint-loader',
-                exclude: [
-                    path.join(__dirname, '../node_modules')
-                ]
-            },
-            {
-                test:/\.jsx$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: [
-                    path.join(__dirname, '../node_modules')
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.join(__dirname, '../client/template.html')
-        })
+if (isDev) {
+  config.entry = {
+    app: [
+      'react-hot-loader/patch',
+      path.join(__dirname, '../client/app.js')
     ]
-};
-if(isDev) {
-    config.entry = {
-        app: [
-            'react-hot-loader/patch',
-            path.join(__dirname, '../client/app.js')
-        ]
+  }
+  config.devServer = {
+    host: '0.0.0.0',
+    port: '8888',
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    overlay: {
+      errors: true
+    },
+    publicPath: '/public/',
+    historyApiFallback: {
+      index: '/public/index.html'
     }
-    config.devServer = {
-        host: '0.0.0.0',
-        port: '8888',
-        contentBase: path.join(__dirname, '../dist'),
-        hot: true,
-        overlay : {
-            errors: true
-        },
-        publicPath:'/public/',
-        historyApiFallback: {
-            index: '/public/index.html'
-        }
-    };
-    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  }
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
-module.exports = config;
+module.exports = config
